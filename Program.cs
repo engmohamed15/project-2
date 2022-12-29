08c03cc701822354dc4c1c409d298e76e3b44a37
engmohamed15 <116379932+engmohamed15@users.noreply.github.com>
2022-12-29 09:45:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace descrit_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inter your frist number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inter your second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for(int num = num1; num < num2; num++) { 
+            decimal sum =0;
+                for(int i = 1 ; i<num ; i++)
+                {
+                    if(num%i==0) { sum = sum + i; }
+                }
+                if (sum == num) { Console.WriteLine(num); }
+                Console.ReadKey();
+            
+            }
+        }
+    }
+}
